c726dffdf5f777a457d5df6a09587f3cde737cd5
shubham shrimali <shubham.shrimali2015@vit.ac.in>
2017-08-06 18:13:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+   res.send("Article one sereved here"); 
+});
+
+app.get('/article-two',function(req,res){
+   res.send("Article two sereved here"); 
+});
+
+app.get('/article-three',function(req,res){
+   res.send("Article three sereved here"); 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
